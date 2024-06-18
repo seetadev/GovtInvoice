04d1a94d4fd716407d021727fdaccec132a0670d
Manas Madan <manasmadan08@gmail.com>
2024-06-18 17:11:04 +0530

    Week 1

--- Govt-Billing-React/README.md
+++ Govt-Billing-React/README.md
@@ -1,6 +1,101 @@
-1. npm i -g @ionic/cli
-2. git clone https://github.com/geetanshu2502/ionicReactApp
-3. cd ionicReactApp
-4. npm install
-5. ionic serve (With tis you can access the app in browser)
-6. ionic capacitor open android(opens android studio with the application, build and run)
+# Project Migration and Fixes
+
+This project involves the migration of an Ionic v5 app with outdated dependencies to Ionic v7 using Vite as the build tool. The process included transferring code and addressing various issues encountered during the migration.
+
+## Migration Steps
+
+- Create a new Ionic Vite app:
+
+```bash
+npx create-ionic-vite@latest
+```
+
+- Transfer code from the original Ionic v5 project to the new Vite-based project.
+
+- Fix errors encountered during the migration process by addressing them one by one.
+
+## Integration of SocialCalc
+
+During the integration of SocialCalc, an issue arose when switching from UMD to ES6 imports. Although the code worked on the web, it failed on an Android emulator due to the unavailability of the window object.
+
+To resolve this, the UMD module was re-implemented, and the window object was defined in the SocialCalc file. Additionally, the SocialCalc file's outdated code lacked variable declarations. To make these variables available at the top of the scope, the var keyword was used.
+
+## Android Emulator Compatibility Fix
+
+To make the project compatible with an Android emulator, ensure that variables are declared at the top of the scope in the SocialCalc file. This step is essential for addressing issues related to the unavailability of the window object on the Android platform.
+
+## Running the Project on Web
+
+To build an APK from the codebase, follow these steps:
+
+- Install Node.js if not already installed.
+
+- Clone the repository:
+
+```bash
+git clone REPO_URL
+```
+
+- Navigate to the project directory:
+
+```bash
+cd REPO_NAME
+```
+
+- Install project dependencies:
+
+```bash
+npm install
+```
+
+- Install the Ionic CLI globally:
+
+```bash
+npm install -g @ionic/cli
+```
+
+- Setup a firebase project and add the firebase configuration in the `.env` file in the format given in the `.env.example`
+
+- Serve the application:
+
+```bash
+ionic serve
+```
+
+These steps will set up the project and allow you to test it in a development environment.
+
+## Running the Project on Android Device
+
+- Install Android Studio if not already installed.
+
+- Sync android codebase
+
+```bash
+ionic cap sync android
+```
+
+- Opening the Project in android Studio
+
+```bash
+ionic cap open android
+```
+
+Now you can run the app on a physical device or a virtual emulator, you can also build the app from the menu bar
+
+## Running the Project on IOS Device
+
+- Install XCode and XCode CLI if not already installed.
+
+- Sync ios codebase
+
+```bash
+ionic cap sync ios
+```
+
+- Opening the Project in XCode
+
+```bash
+ionic cap open ios
+```
+
+Now you can run the app on a physical device or a virtual emulator, you can also build the app from the menu bar
---
+++ Govt-Billing-React/src/pages/Home.tsx
@@ -0,0 +1,161 @@
+import {
+  IonButton,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonPopover,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { APP_NAME, DATA } from "../app-data";
+import Login from "../components/Login/Login";
+import * as AppGeneral from "../components/socialcalc/index.js";
+import { useEffect, useState } from "react";
+import { Local } from "../components/Storage/LocalStorage";
+import { menu, settings } from "ionicons/icons";
+import "./Home.css";
+import Menu from "../components/Menu/Menu";
+import Files from "../components/Files/Files";
+import NewFile from "../components/NewFile/NewFile";
+import { initFirebase } from "../firebase/index";
+
+const Home: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showPopover, setShowPopover] = useState<{
+    open: boolean;
+    event: Event | undefined;
+  }>({ open: false, event: undefined });
+  const [selectedFile, updateSelectedFile] = useState("default");
+  const [billType, updateBillType] = useState(1);
+  const [device] = useState("default");
+
+  initFirebase();
+
+  const store = new Local();
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+  const activateFooter = (footer) => {
+    AppGeneral.activateFooterButton(footer);
+  };
+
+  useEffect(() => {
+    const data = DATA["home"][device]["msc"];
+    AppGeneral.initializeApp(JSON.stringify(data));
+  }, []);
+
+  useEffect(() => {
+    activateFooter(billType);
+  }, [billType]);
+
+  const footers = DATA["home"][device]["footers"];
+  const footersList = footers.map((footerArray) => {
+    return (
+      <IonButton
+        key={footerArray.index}
+        expand="full"
+        color="light"
+        className="ion-no-margin"
+        onClick={() => {
+          updateBillType(footerArray.index);
+          activateFooter(footerArray.index);
+          setShowPopover({ open: false, event: undefined });
+        }}
+      >
+        {footerArray.name}
+      </IonButton>
+    );
+  });
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonTitle>{APP_NAME}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonToolbar color="primary">
+          <Login />
+
+          <IonIcon
+            icon={settings}
+            slot="end"
+            className="ion-padding-end"
+            size="large"
+            onClick={(e) => {
+              setShowPopover({ open: true, event: e.nativeEvent });
+              console.log("Popover clicked");
+            }}
+          />
+          <Files
+            filesFrom="Local"
+            store={store}
+            file={selectedFile}
+            updateSelectedFile={updateSelectedFile}
+            updateBillType={updateBillType}
+          />
+          <Files
+            filesFrom="Cloud"
+            store={store}
+            file={selectedFile}
+            updateSelectedFile={updateSelectedFile}
+            updateBillType={updateBillType}
+          />
+
+          <NewFile
+            file={selectedFile}
+            updateSelectedFile={updateSelectedFile}
+            store={store}
+            billType={billType}
+          />
+          <IonPopover
+            animated
+            keyboardClose
+            backdropDismiss
+            event={showPopover.event}
+            isOpen={showPopover.open}
+            onDidDismiss={() =>
+              setShowPopover({ open: false, event: undefined })
+            }
+          >
+            {footersList}
+          </IonPopover>
+        </IonToolbar>
+        <IonToolbar color="secondary">
+          <IonTitle className="ion-text-center">
+            Editing : {selectedFile}
+          </IonTitle>
+        </IonToolbar>
+
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton type="button" onClick={() => setShowMenu(true)}>
+            <IonIcon icon={menu} />
+          </IonFabButton>
+        </IonFab>
+
+        <Menu
+          showM={showMenu}
+          setM={closeMenu}
+          file={selectedFile}
+          updateSelectedFile={updateSelectedFile}
+          store={store}
+          bT={billType}
+        />
+
+        <div id="container">
+          <div id="workbookControl"></div>
+          <div id="tableeditor"></div>
+          <div id="msg"></div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
